3313e3b4d3a4988455945b9cbc19453d6238e086
pcysl5edgo <31692496+pCYSl5EDgo@users.noreply.github.com>
2026-01-04 16:47:20 +0900

    Rewriter is appropriate

--- Pcysl5edgo.GitRevisionBuilder.BenchmarkDotNet.SourceGenerator/Rewriter.cs
+++ Pcysl5edgo.GitRevisionBuilder.BenchmarkDotNet.SourceGenerator/Rewriter.cs
@@ -4,7 +4,7 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Pcysl5edgo.GitRevisionBuilder.BenchmarkDotNet.SourceGenerator;
 
-internal sealed class Rewriter : CSharpSyntaxVisitor<SyntaxNode>
+internal sealed class Rewriter : CSharpSyntaxRewriter
 {
     public string From = "global";
     public string To = "global";
